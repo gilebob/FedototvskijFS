4d827d540e490db606d362f87e2b76babdebe3e9
gilebob <qsxfthnko123qsxfthnko@gmail.com>
2024-10-25 20:20:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+// Подключение файла соединения с БД
+include_once 'db.class.php';
+
+// хост БД
+define('db_host','localhost');
+
+// Имя БД
+define('db_name','FedotovskijFS');
+
+// Пользователь БД
+define('db_user','admin');
+
+// Пароль БД
+define('db_pass','admin_password');
+
+// Обявление класса для подключения к бд
+$db = new DB_class(db_host,db_FedotovskijFS,db_admin,db_admin_password);
